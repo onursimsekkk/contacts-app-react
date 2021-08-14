2d5ed3bd1c49d8c7dc9fb80bffa7253de85e4ec4
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-14 14:42:02 +0300

    Add Contacts comp

---
+++ src/components/Contacts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from './List';
+import Form from './Form';
+
+function Contacts() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <List />
+      <Form />
+    </div>
+  )
+}
+
+export default Contacts;
