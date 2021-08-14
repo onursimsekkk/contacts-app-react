2832a00db1ddbf85a0473f2b682a1035502507af
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-14 16:09:29 +0300

    Add props: setContacts, contacs

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+// Import Components
 import List from './List';
 import Form from './Form';
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div>
       <h1>Contacts</h1>
-      <List />
-      <Form />
+      <List contacts={contacts} />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   )
 }
