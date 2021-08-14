c7c91b0c976420b75a2538c7f46dab7197bd49d2
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-14 17:29:49 +0300

    Add filter function

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useState } from "react";
 
-function List() {
+function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((k) => 
+      item[k].toString().toLowerCase().includes(filterText.toLowerCase())
+    )
+  })
+
+  console.log(filtered);
+
   return (
     <div>
-      List
+      <input
+        type="text"
+        placeholder="Filter Contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+      <ul>
+        {filtered.map((contact, index) => (
+          <li key={index}>{contact.fullname}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default List;
