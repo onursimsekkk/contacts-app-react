ffc20efbe6a827c7f93cb020a10b4f17fb4e1344
Onur Simsek <onur.simsekk3@gmail.com>
2021-08-14 16:08:31 +0300

    Add add&remove contacts func

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-function Form() {
-  const [form, setForm]  =useState({fullname: "", phone_number: ""});
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState({ fullname: "", phone_number: "" });
+
+  useEffect(() => {
+    setForm({ fullname: "", phone_number: "" });
+  }, [contacts]);
 
   const onChangeInput = (e) => {
-    setForm({...form, [e.target.name]: e.target.value })
-  }
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = () => {
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
 
   return (
     <div>
       <div>
-        <input 
-          name="fullname" 
+        <input
+          name="fullname"
           placeholder="Full Name"
+          value={form.fullname}
           onChange={onChangeInput}
         />
       </div>
       <div>
-        <input 
-          name="phone_number" 
+        <input
+          name="phone_number"
           placeholder="Phone Number"
+          value={form.phone_number}
           onChange={onChangeInput}
         />
       </div>
       <h3>{}</h3>
       <div>
-        <button>Add</button>
+        <button onClick={onSubmit}>Add</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Form;
